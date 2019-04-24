23a1e51e78bd65c19ca3846af3fc5030afb41ab8
Robyn <oddert@gmail.com>
2019-04-24 12:52:20 +0100

    added mobile nav menu

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,33 @@
 
-const navLogo = document.querySelector('.Nav__logo')
+
+const navLogo = document.querySelector('.Nav--logo')
+const nav = document.querySelector('.Nav')
+const navToggle = document.querySelector('.Nav__toggle')
+
+let navDropOpen = false
+
+function toggleNavDrop (open) {
+  if (open) navDropOpen = true
+  else navDropOpen = !navDropOpen
+  if (navDropOpen && !nav.classList.contains('open')) nav.classList.add('open')
+  if (!navDropOpen && nav.classList.contains('open')) nav.classList.remove('open')
+}
 
 function scrollTop (e) {
   e.preventDefault()
   window.scrollTo(0, 50)
 }
 
-function overrideLogoButton () {
+function initOverrideLogoButton () {
   const link = navLogo.querySelector('a')
   link.href = ''
   link.addEventListener('click', scrollTop)
 }
 
 function init() {
-  overrideLogoButton()
+  initOverrideLogoButton()
 }
+
+navToggle.onclick = e => toggleNavDrop()
 
 document.addEventListener('DOMContentLoaded', init)
