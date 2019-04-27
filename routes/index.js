3c30ac31d214dd4e8a1fb0a58312d977e941ffdd
Robyn <oddert@gmail.com>
2019-04-28 00:59:40 +0100

    added styling for the social penetration theory section

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
 router.route('/')
-  .get((req, res, next) => res.render('index'))
+  .get((req, res, next) => res.render('index', { game_name: 'Qubes', brick_single: 'Qube', brick_multiple: 'Qubes', brick_prefix: 'a' }))
 
 module.exports = router
