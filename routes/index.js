f476f5fff39a977c920cd9667b34e11237bc502e
Robyn <oddert@gmail.com>
2019-04-28 12:45:44 +0100

    added scaffolding for contact section, readjusted futures section

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 const router = require('express').Router()
 
 router.route('/')
-  .get((req, res, next) => res.render('index', { game_name: 'Qubes', brick_single: 'Qube', brick_multiple: 'Qubes', brick_prefix: 'a' }))
+  .get((req, res, next) =>
+    res.render('index', {
+      game_name: 'Qubes',
+      brick_single: 'Qube',
+      brick_multiple: 'Qubes',
+      brick_prefix: 'a',
+      updates: null 
+    })
+  )
 
 module.exports = router
