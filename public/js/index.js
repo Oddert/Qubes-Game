93642c6443d7848f6f4fd66984423705d8acaf6d
Robyn <oddert@gmail.com>
2019-05-02 15:07:31 +0100

    added contact section fully

--- public/js/index.js
+++ public/js/index.js
@@ -31,3 +31,38 @@ function init() {
 navToggle.onclick = e => toggleNavDrop()
 
 document.addEventListener('DOMContentLoaded', init)
+
+
+
+
+
+const flashNotif = str => {
+  const notif = document.querySelector('.notifier')
+  notif.hidden = false
+  notif.querySelector('span').textContent = str
+  notif.classList.add('show')
+  setTimeout(() => {
+    notif.classList.add('fade')
+    setTimeout(() => {
+      notif.classList.remove('show', 'fade')
+      setTimeout(() => notif.hidden = true, 300)
+    }, 1000)
+  }, 2000)
+}
+
+const copy = str => {
+  const shadow_input = document.createElement('textarea')
+  shadow_input.value = str
+  document.body.appendChild(shadow_input)
+  shadow_input.select()
+  document.execCommand("copy")
+  document.body.removeChild(shadow_input)
+  flashNotif(shadow_input.value)
+  console.log('Copied text:', shadow_input.value)
+}
+
+const copyLinkButton = document.querySelector('.social.link')
+
+if (copyLinkButton) {
+  copyLinkButton.onclick = e => copy(e.target.closest('.social.link').dataset.link)
+}
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 const router = require('express').Router()
 
 router.route('/')
-  .get((req, res, next) =>
-    res.render('index', {
-      game_name: req.query.name ? req.query.name : 'Bloqs',
-      brick_single: req.query.object ? req.query.object : 'Bloq',
-      brick_multiple: req.query.object ? req.query.object + 's' : 'Bloqs',
-      brick_prefix: 'a',
-      updates: null
-    })
+  .get((req, res, next) => {
+      console.log(process.env.LOCATION ? process.env.LOCATION : 'https://oddert-mos-matterial-vis.glitch.me')
+      res.render('index', {
+        game_name: req.query.name ? req.query.name : 'Bloqs',
+        brick_single: req.query.object ? req.query.object : 'Bloq',
+        brick_multiple: req.query.object ? req.query.object + 's' : 'Bloqs',
+        brick_prefix: 'a',
+        updates: null,
+        location: process.env.LOCATION ? process.env.LOCATION : 'https://oddert-mos-matterial-vis.glitch.me'
+      })
+    }
   )
 
 router.route('/d3')
